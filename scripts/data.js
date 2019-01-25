c3105018f180ee268671199b34bbd2fefe383108
naren <naren.yellavula@tradebyte.com>
2019-01-25 21:45:59 +0100

    added table on scene

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+// This class stores data for locating sprites on scenes
+class GameData {
+    constructor () {
+        this.TABLE_X = 630;
+        this.TABLE_Y = 690;
+    }
+}
+
+let game_data = new GameData();
--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,13 @@
-class Game {
+class MainGame {
     constructor () {
         this.config = {
             type: Phaser.AUTO,
-            width: 800,
-            height: 600,
+            width: 1366,
+            height: 768,
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: { y: 200 }
+                    gravity: { y: 0 }
                 }
             },
             scene: {
@@ -18,33 +18,14 @@ class Game {
     }
 
     preload () {
-        this.load.setBaseURL('http://labs.phaser.io');
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.image('logo', 'sprites/table.gif');
     }
 
     create () {
-        this.add.image(400, 300, 'sky');
-    
-        var particles = this.add.particles('red');
-    
-        var emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-    
-        var logo = this.physics.add.image(400, 100, 'logo');
-    
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-    
-        emitter.startFollow(logo);
+        var logo = this.physics.add.image(game_data.TABLE_X, game_data.TABLE_Y, 'logo');
     }
 }
 
 
-let game = new Game();
+let game = new MainGame();
 let start_game = new Phaser.Game(game.config);
