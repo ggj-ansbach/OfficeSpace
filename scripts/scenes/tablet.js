a914a8f3281cd261ed08bbe4afd995d9dbb0e55d
Naren Yellavula <narenarya@live.com>
2019-01-26 21:37:37 +0100

    implement back to main scene from tablet

--- scripts/scenes/tablet.js
+++ scripts/scenes/tablet.js
@@ -3,14 +3,23 @@ class TabletScene extends Phaser.Scene {
     super({key: game_data.scene_list.TABLET});
   }
 
+  onDown () {
+    console.log("Going back to Main Scene");
+    this.scene.start(game_data.scene_list.MAIN, 'hello from table');
+  }
+
   preload () {
     this.load.image('tabletBG', '/assets/images/backgrounds/tablet.png');
     this.load.image('close', '/assets/images/objects/close.png');
   }
 
   create () {
+    console.log("In the Tablet scene");
     this.add.sprite(400 , 300, 'tabletBG');
-    this.add.sprite(700, 80, 'close');
+    let close = this.add.sprite(700, 80, 'close');
+    close.setInteractive();
+    close.on('pointerdown', this.onDown.bind(this));
+    // close.events.onInputDown.add(this.onDown, this);
     console.log('TableScene created');
   }
 }
