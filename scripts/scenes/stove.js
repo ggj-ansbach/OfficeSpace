42f0dead6be65c34290971da09757569825f8984
ejelome <ejelome@gmail.com>
2019-01-27 10:59:47 +0100

    Add remaining scene timers

--- scripts/scenes/stove.js
+++ scripts/scenes/stove.js
@@ -18,6 +18,11 @@ class StoveScene extends Phaser.Scene {
     console.log("In the StoveScene");
     this.add.sprite(400 , 300, 'cook');
 
+    // Add time text:
+    timerHeader = this.add.bitmapText(105, 460, 'carrier_command', 'TIME', 20);
+    timerValue = this.add.bitmapText(100, 510, 'carrier_command', timerInitValue, 40);
+    timerText = this.add.bitmapText(190, 530, 'carrier_command', 's', 20);
+
     // Create close after tablet, otherwise backgroudn may cover close
     let close = this.add.sprite(755, 45, 'close');
     close.setInteractive();
