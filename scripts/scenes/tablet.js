7bf2474594c2c48c823d9e20ede6266cf984cd3a
ejelome <ejelome@gmail.com>
2019-01-27 10:38:45 +0100

    Add tablet timer text

--- scripts/scenes/tablet.js
+++ scripts/scenes/tablet.js
@@ -15,10 +15,14 @@ class TabletScene extends Phaser.Scene {
   }
 
   create () {
-    // Adding tablet and note as a group
     let blocks = this.physics.add.staticGroup();
     blocks.create(400 , 300, 'tablet');
     let note = blocks.create(270, 294, 'note').setScale(0.17, 0.15); // sprite is so big
+
+    // Add time text:
+    timerHeader = this.add.bitmapText(600, 360, 'carrier_command', 'TIME', 20);
+    timerValue = this.add.bitmapText(595, 410, 'carrier_command', timerInitValue, 40);
+    timerText = this.add.bitmapText(685, 430, 'carrier_command', 's', 20);
     
     // Create close after tablet, otherwise backgroudn may cover close
     let close = this.add.sprite(755, 45, 'close');
